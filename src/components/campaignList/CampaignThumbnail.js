74bd4e520d86c623a030898f4fe5955980d52841
ProcessX <luc2526@gmail.com>
2021-03-13 17:32:03 +0100

    Thumbnails stylised on going

--- src/components/campaignList/CampaignThumbnail.js
+++ src/components/campaignList/CampaignThumbnail.js
@@ -5,9 +5,23 @@ import ThumbnailOptions from "./ThumbnailOptions";
 const CampaignThumbnail = ({ campaign }) => {
   const LINKURL = "/campaign-details?id=";
 
+  const getStatusClassname = () => {
+    let classname = "campaign__status";
+    if (campaign.details.status) {
+      classname += ` ${classname}--${campaign.details.status.toLowerCase()}`;
+    }
+    return classname;
+  };
+
+  const getStatusCapitilized = () => {
+    let status = campaign.details.status;
+    status = status.toLowerCase();
+    return status.charAt(0).toUpperCase() + status.slice(1);
+  };
+
   return (
     <Link to={`${LINKURL}${campaign.id.value}`} className={"campaignThumbnail"}>
-      <p className={"campaign__status"}>{campaign.details.status}</p>
+      <p className={getStatusClassname()}>{getStatusCapitilized()}</p>
       <h3 className={"campaign__name"}>{campaign.details.name}</h3>
       <p className={"campaign__id"}>{campaign.id.value}</p>
       <ThumbnailOptions />
