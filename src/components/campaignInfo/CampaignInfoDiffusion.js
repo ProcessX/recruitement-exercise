1bf45ea743f0b3547a32cf436c872d394e08b228
ProcessX <luc2526@gmail.com>
2021-03-14 11:22:08 +0100

    Reset SlotsBlock - New direction

--- src/components/campaignInfo/CampaignInfoDiffusion.js
+++ src/components/campaignInfo/CampaignInfoDiffusion.js
@@ -4,10 +4,19 @@ import SingleBlock from "./infoBlocks/SingleBlock";
 import SlotsBlock from "./infoBlocks/SlotsBlock";
 
 const CampaignInfoDiffusion = ({ diffusion }) => {
+  const dateFormatting = (date) => {
+    console.log("Get formatted date");
+    const dateFormatted = new Date(date);
+    return `${dateFormatted.getDay()} ${dateFormatted.getMonth()} ${dateFormatted.getFullYear()}`;
+  };
+
   return (
     <CampaignInfo title={"Diffusion"} classname={"diffusion"}>
-      <SingleBlock title={"From"} value={"value"} />
-      <SingleBlock title={"To"} value={"value"} />
+      <SingleBlock
+        title={"From"}
+        value={dateFormatting(diffusion.period.from)}
+      />
+      <SingleBlock title={"To"} value={dateFormatting(diffusion.period.to)} />
       <SlotsBlock diffusion={diffusion} />
     </CampaignInfo>
   );
