222257fdf8707c534bf8ed3d21a525ea9bfe7397
ProcessX <luc2526@gmail.com>
2021-03-14 10:51:25 +0100

    GO BACK ! The slots are hours, not days

--- src/components/campaignInfo/infoBlocks/SlotsBlock.js
+++ src/components/campaignInfo/infoBlocks/SlotsBlock.js
@@ -1,18 +1,38 @@
 import React, { useState } from "react";
 import InfoBlock from "./InfoBlock";
 import "react-modern-calendar-datepicker/lib/DatePicker.css";
-//import DatePicker from "react-modern-calendar-datepicker";
 import { Calendar } from "react-modern-calendar-datepicker";
 
 const SlotsBlock = ({ diffusion }) => {
   const [selectedDay, setSelectedDay] = useState([]);
 
+  const dateFormatting = (date) => {
+    console.log("Get formatted date");
+    const dateFormatted = new Date(date);
+    return {
+      year: dateFormatted.getFullYear(),
+      month: dateFormatted.getMonth(),
+      day: dateFormatted.getDay(),
+    };
+  };
+
+  const slotFormatting = (slot) => {
+    return null;
+  };
+
+  const getDateArray = () => {
+    return [];
+  };
+
   return (
     <InfoBlock title={"Slots"} className={"slotsBlock"}>
       <p>Calendar</p>
+      <p>{diffusion.period.to}</p>
       <Calendar
         value={selectedDay}
         onChange={setSelectedDay}
+        minimumDate={dateFormatting(diffusion.period.from)}
+        maximumDate={dateFormatting(diffusion.period.to)}
         shouldHighlightWeekends
       />
     </InfoBlock>
