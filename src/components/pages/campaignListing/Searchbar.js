b092aef3b8aa99287a51a7c1fbfbeb0a82b34a77
ProcessX <luc2526@gmail.com>
2021-03-15 14:04:02 +0100

    Search bar prestyled OK

--- src/components/pages/campaignListing/Searchbar.js
+++ src/components/pages/campaignListing/Searchbar.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { createRef } from "react";
 
 const Searchbar = ({ searchName }) => {
-  var searchTimer;
   const searchDelay = 500;
+  var searchTimer;
+  let inputRef = createRef();
 
   const onChange = (e) => {
     clearTimeout(searchTimer);
     searchTimer = setTimeout(() => searchName(e.target.value), searchDelay);
   };
 
+  const focusInput = () => {
+    inputRef.current.focus();
+  };
+
   return (
-    <div className={"searchbar"}>
-      <input type="test" placeholder="Search" onChange={onChange} />
+    <div
+      className={"controller controller--searchbar"}
+      onClick={() => focusInput()}
+    >
+      <input
+        type="test"
+        placeholder="Search"
+        onChange={onChange}
+        ref={inputRef}
+      />
     </div>
   );
 };
