f7f53188511c9166efb708d3a50ebc86b38642ae
ProcessX <luc2526@gmail.com>
2021-03-15 18:58:09 +0100

    Problem with slotsArray

--- src/components/campaignInfo/infoBlocks/slotsElem/Slot.js
+++ src/components/campaignInfo/infoBlocks/slotsElem/Slot.js
@@ -2,8 +2,11 @@ import React from "react";
 
 const Slot = ({ timeRange, value, updateSlot }) => {
   return (
-    <div className={"slot"} onClick={() => updateSlot(timeRange)}>
-      <p className={"slot__state"}>{`` + value}</p>
+    <div
+      className={`slot ${value ? " slot--checked" : ""}`}
+      onClick={() => updateSlot(timeRange)}
+    >
+      <span className={"slot__state"}>{`` + value}</span>
     </div>
   );
 };
