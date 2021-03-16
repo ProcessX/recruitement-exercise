2541bbf4cdc8205eef5077c601fb93f60234c0c7
ProcessX <luc2526@gmail.com>
2021-03-16 09:39:11 +0100

    Slots may be improved

--- src/components/campaignInfo/infoBlocks/slotsElem/Slot.js
+++ src/components/campaignInfo/infoBlocks/slotsElem/Slot.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ReactComponent as Checked } from "../../../../assets/icons/check.svg";
 
 const Slot = ({ timeRange, value, updateSlot }) => {
   return (
@@ -6,7 +7,9 @@ const Slot = ({ timeRange, value, updateSlot }) => {
       className={`slot ${value ? " slot--checked" : ""}`}
       onClick={() => updateSlot(timeRange)}
     >
-      <span className={"slot__state"}>{`` + value}</span>
+      <span className={"slot__state"}>
+        <Checked />
+      </span>
     </div>
   );
 };
